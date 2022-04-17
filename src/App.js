0bc4b5e0fc277af2a2c06d530c654eb709bc4a3d
Santiago castrillon <santo.castrillon@gmail.com>
2022-04-16 21:27:03 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Venta de Automoviles</h1>
+    <strong>Vehiculos disponibles</strong>
+    <li>Maseratti</li>
+    <li>Ferrari</li>
+    <li>Aston Martin</li>
+    <li>Lamborghini</li>
+  </>
+  );
+}
+
+export default App;
